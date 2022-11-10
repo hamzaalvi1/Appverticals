cf19d06afe948e9c3119cccea74c9b1482012594
Shoaib Zafar <shoaib.zafar@koderlabs.com>
2022-11-10 20:19:32 +0500

    develop footer component 2

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import { Container, Row, Col } from "reactstrap";
 import styles from "./Footer.module.scss"
 import Image from "next/image";
+import Link from "next/link";
 
 function Footer() {
   return (
@@ -14,25 +15,25 @@ function Footer() {
                             <h4>Technologies</h4>
                             <div className={styles["footerUlFirst"]}>
                                 <ul>
-                                    <li> <a href="/ios-app-development/">iOS</a></li>
-                                    <li> <a href="/android-app-development/"> Android </a></li>
-                                    <li> <a href="/xamarin-app-development/"> Xamarian  </a></li>
-                                    <li> <a href="/react-native-app-development/"> React Native  </a></li>
-                                    <li> <a href="/ionic-app-development/"> Ionic  </a></li>
-                                    <li> <a href="/flutter-development/"> Flutter </a></li>
+                                    <li> <Link href="/ios-app-development/">iOS</Link></li>
+                                    <li> <Link href="/android-app-development/"> Android </Link></li>
+                                    <li> <Link href="/xamarin-app-development/"> Xamarian  </Link></li>
+                                    <li> <Link href="/react-native-app-development/"> React Native  </Link></li>
+                                    <li> <Link href="/ionic-app-development/"> Ionic  </Link></li>
+                                    <li> <Link href="/flutter-development/"> Flutter </Link></li>
                                 </ul>
                                 <ul>
-                                    <li> <a href="/angular-development/"> Angular </a></li>
-                                    <li> <a href="/javascript-development/"> Javascript </a></li>
-                                    <li> <a href="/react-js-development/"> ReactJS </a></li>
-                                    <li> <a href="/vue-js-development/"> Vue.js </a></li>
-                                    <li> <a href="/java-app-development/"> Java </a></li>
-                                    <li> <a href="/node-js-development/"> Node.js </a></li>
+                                    <li> <Link href="/angular-development/"> Angular </Link></li>
+                                    <li> <Link href="/javascript-development/"> Javascript </Link></li>
+                                    <li> <Link href="/react-js-development/"> ReactJS </Link></li>
+                                    <li> <Link href="/vue-js-development/"> Vue.js </Link></li>
+                                    <li> <Link href="/java-app-development/"> Java </Link></li>
+                                    <li> <Link href="/node-js-development/"> Node.js </Link></li>
                                 </ul>
                                 <ul>
-                                    <li> <a href="/.net-development/"> ASP.NET </a></li>
-                                    <li> <a href="/python-development/"> Python </a></li>
-                                    <li> <a href="/php-development/"> PHP  </a></li>
+                                    <li> <Link href="/.net-development/"> ASP.NET </Link></li>
+                                    <li> <Link href="/python-development/"> Python </Link></li>
+                                    <li> <Link href="/php-development/"> PHP  </Link></li>
                                 </ul>
                             </div>
                         </div>
@@ -41,12 +42,12 @@ function Footer() {
                             <h4>Core Services</h4>
                             <div className={styles["footerUlSec"]}>
                                 <ul>
-                                    <li> <a href="/web-application-development/"> Web Application Development </a></li>
-                                    <li> <a href="/mobile-app-development-company/"> Mobile App Development  </a></li>
-                                    <li> <a href="/frontend-development/"> Front End Development  </a></li>
-                                    <li> <a href="/hire-dedicated-developer/"> Hire  Dedicated Developer  </a></li>
-                                    <li> <a href="/product-prototyping/"> Product Prototyping</a></li>
-                                    <li> <a href="/cloud-development/"> Cloud & DevOps </a></li>
+                                    <li> <Link href="/web-application-development/"> Web Application Development </Link></li>
+                                    <li> <Link href="/mobile-app-development-company/"> Mobile App Development  </Link></li>
+                                    <li> <Link href="/frontend-development/"> Front End Development  </Link></li>
+                                    <li> <Link href="/hire-dedicated-developer/"> Hire  Dedicated Developer  </Link></li>
+                                    <li> <Link href="/product-prototyping/"> Product Prototyping</Link></li>
+                                    <li> <Link href="/cloud-development/"> Cloud & DevOps </Link></li>
                                 </ul>
                             </div>
                         </div>
@@ -55,21 +56,21 @@ function Footer() {
                             <h4>Hire Developers</h4>
                             <div className = {`${styles["footerUlFirst"]} ${styles["_hire-dev"]}`}>
                                 <ul>
-                                    <li> <a href="/hire-php-developer/"> Hire PHP Developers </a></li>
-                                    <li> <a href="/hire-nodejs-developer/"> Hire Node.js Developers </a></li>
-                                    <li> <a href="/hire-codeigniter-developer/"> Hire Codeigniter Developers </a></li>
-                                    <li> <a href="/hire-laravel-developer/"> Hire Laravel Developers</a></li>
-                                    <li> <a href="/hire-wordpress-developer/"> Hire WordPress Developers  </a></li>
-                                    <li> <a href="/hire-kotlin-developer/"> Hire Kotlin Developers </a></li>
+                                    <li> <Link href="/hire-php-developer/"> Hire PHP Developers </Link></li>
+                                    <li> <Link href="/hire-nodejs-developer/"> Hire Node.js Developers </Link></li>
+                                    <li> <Link href="/hire-codeigniter-developer/"> Hire Codeigniter Developers </Link></li>
+                                    <li> <Link href="/hire-laravel-developer/"> Hire Laravel Developers</Link></li>
+                                    <li> <Link href="/hire-wordpress-developer/"> Hire WordPress Developers  </Link></li>
+                                    <li> <Link href="/hire-kotlin-developer/"> Hire Kotlin Developers </Link></li>
 
                                 </ul>
                                 <ul>
-                                    <li> <a href="/hire-android-developer/"> Hire Android Developers </a></li>
-                                    <li> <a href="/hire-shopify-developer/"> Hire Shopify Developers </a></li>
-                                    <li> <a href="/hire-reactjs-developer/"> Hire ReactJS Developers </a></li>
-                                    <li> <a href="/hire-vuejs-developer/"> Hire Vue.js Developers</a></li>
-                                    <li> <a href="/hire-xamarin-developer/"> Hire Xamarin Developers  </a></li>
-                                    <li> <a href="/hire-angular-developer/"> Hire Angular Developers  </a></li>
+                                    <li> <Link href="/hire-android-developer/"> Hire Android Developers </Link></li>
+                                    <li> <Link href="/hire-shopify-developer/"> Hire Shopify Developers </Link></li>
+                                    <li> <Link href="/hire-reactjs-developer/"> Hire ReactJS Developers </Link></li>
+                                    <li> <Link href="/hire-vuejs-developer/"> Hire Vue.js Developers</Link></li>
+                                    <li> <Link href="/hire-xamarin-developer/"> Hire Xamarin Developers  </Link></li>
+                                    <li> <Link href="/hire-angular-developer/"> Hire Angular Developers  </Link></li>
                                 </ul>
                             </div>
                         </div>
@@ -78,14 +79,14 @@ function Footer() {
 
                     <div className={styles["footerDisc"]}>
                         <div className={styles["footerDiscLogo"]}>
-                            <a href="/">
+                            <Link href="/">
                             <Image
                                 src={"/footer-logo.webp"}
                                 width={300}
                                 height={40}
                                 alt="logo"
                                 />
-                            </a>
+                            </Link>
                         </div>
                         <div className={styles["footerContent"]}>
                             <p>Our strength lies in the comprehensive suite of services we offer to our clients. From full-stack development to just consultancy- you demand, we deliver. From full-stack development to just consultancy-you demand, we deliver. consultancy-you demand, we deliver.</p>
@@ -95,14 +96,14 @@ function Footer() {
 
                     <div className={styles["footerCopyRight"]}>
                         <div className={styles["footerCopyRightLeft"]}>
-                            <p>© 2022 <a href="/">AppVerticals</a>. ALL RIGHTS RESERVED.</p>
+                            <p>© 2022 <Link href="/">AppVerticals</Link>. ALL RIGHTS RESERVED.</p>
                         </div>
                         <div  className={styles["footerCopyRightSide"]}>
                             <ul>
-                                <li><a href="/privacy-policy/"> Privacy Policy   </a></li>
-                                <li><a href="/terms-of-services/"> Terms of Use   </a></li>
+                                <li><Link href="/privacy-policy/"> Privacy Policy   </Link></li>
+                                <li><Link href="/terms-of-services/"> Terms of Use   </Link></li>
                             
-                                <li><a href="javascript:;"> Sitemap </a></li>
+                                <li><Link href="javascript:;"> Sitemap </Link></li>
                             </ul>
                         </div>
                     </div>
